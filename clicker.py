0e77582f6f95f2c8ad7020f2a474a875d944a566
Witut2005 <wiktor.glogowski@onet.pl>
2022-07-15 19:01:48 +0200

    click click click

--- clicker.py
+++ clicker.py
@@ -7,9 +7,23 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('-block', help='blocks cursor', action='store_true')
 parser.add_argument('-delay', help='click delay', type=float, default=0, required=False)
-parser.add_argument('-key', help='key to start and end autoclicker loop', default=keyboard.Key.ctrl_l, required=False)
+parser.add_argument('-key', help='key to start and end autoclicker loop', default='ctrl', required=False)
+
+available_keys = ('alt', 'alt_gr', 'alt_l', 'alt_r', 'backspace', 'caps_lock', 'cmd', 'cmd_l', 'cmd_r',
+                  'ctrl', 'ctrl_l', 'ctrl_r', 'delete', 'down', 'end', 'enter', 'esc', 'f1', 'home', 'insert',
+                  'left', 'media_next', 'media_play_pause', 'media_previous', 'media_volume_mute', 'media_volume_up',
+                  'menu', 'num_lock', 'page_down', 'page_up', 'pause', 'print_screen', 'right', 'scroll_lock', 'shift',
+                  'shift_l', 'shift_r', 'space', 'tab', 'up')
 
 args = parser.parse_args()
+
+try:
+    available_keys.index(args.key)
+except ValueError:
+    print("error: you can't use this key", args.key)
+    print('list of available keys:')
+    print(available_keys)
+    sys.exit()
 
 args.key = 'Key.' + args.key
 print(args.key)
@@ -39,6 +53,7 @@ mouse_listener = Listener(on_move=on_move, on_click=on_click)
 mouse_listener.start()
 
 
+# KEYBOARD
 def on_press(key):
     global keyboard_last_key
     keyboard_last_key = key
@@ -53,6 +68,7 @@ keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 keyboard_listener.start()
 
 while True:
+    print(keyboard_last_key, args.key)
     if str(keyboard_last_key) == args.key:
         start_position = Mouse.position
         keyboard_last_key = None
