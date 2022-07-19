95e046043d42859f8ffa83f6e7ccb6af2c925b58
Witut2005 <wiktor.glogowski@onet.pl>
2022-07-19 16:17:42 +0200

    clicker update

--- clicker.py
+++ clicker.py
@@ -3,11 +3,13 @@ from pynput.mouse import Listener, Controller, Button
 from pynput import keyboard
 import argparse
 import time
+import signal
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-block', help='blocks cursor', action='store_true')
 parser.add_argument('-delay', help='click delay', type=float, default=0, required=False)
 parser.add_argument('-key', help='key to start and end autoclicker loop', default='ctrl', required=False)
+parser.add_argument('-points', help='how many points do you want to select', default=1, type=int, required=False)
 
 available_keys = ('alt', 'alt_gr', 'alt_l', 'alt_r', 'backspace', 'caps_lock', 'cmd', 'cmd_l', 'cmd_r',
                   'ctrl', 'ctrl_l', 'ctrl_r', 'delete', 'down', 'end', 'enter', 'esc', 'f1', 'home', 'insert',
@@ -16,6 +18,9 @@ available_keys = ('alt', 'alt_gr', 'alt_l', 'alt_r', 'backspace', 'caps_lock', '
                   'shift_l', 'shift_r', 'space', 'tab', 'up')
 
 args = parser.parse_args()
+
+if args.block and args.points > 1:
+    sys.exit('block and points(more than 1) options cannot be specified together')
 
 try:
     available_keys.index(args.key)
@@ -37,10 +42,12 @@ keyboard_last_key = None
 print(args.block)
 print(args.delay)
 
+point_list = []
+point_counter = len(point_list) - 1
 
-# MOUSE
+
 def on_move(x, y):
-    if args.block and start_position is not None:
+    if args.block and point_list[0] is not None:
         Mouse.position = start_position
 
 
@@ -50,13 +57,11 @@ def on_click(x, y, button, pressed):
 
 
 mouse_listener = Listener(on_move=on_move, on_click=on_click)
-mouse_listener.start()
 
-
-# KEYBOARD
 def on_press(key):
     global keyboard_last_key
-    keyboard_last_key = key
+    if keyboard_last_key is None:
+        keyboard_last_key = key
 
 
 def on_release(key):
@@ -67,17 +72,34 @@ def on_release(key):
 keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 keyboard_listener.start()
 
-while True:
-    print(keyboard_last_key, args.key)
-    if str(keyboard_last_key) == args.key:
-        start_position = Mouse.position
-        keyboard_last_key = None
-        break
+for point in range(0, args.points):
+    while True:
+        if str(keyboard_last_key) == args.key:
+            start_position = Mouse.position
+            keyboard_last_key = None
+            point_list.append(Mouse.position)
+            break
+
+print(point_list)
+point_counter = len(point_list)
+print(point_counter)
+mouse_listener.start()
+
 
 while True:
-    time.sleep(args.delay)
+
+    try:
+        time.sleep(args.delay)
+    except KeyboardInterrupt:
+        sys.exit('exit')
+
 
     if str(keyboard_last_key) == args.key:
         sys.exit('exit')
 
-    Mouse.click(Button.left, 1)
+    if point_counter > 1:
+        for x in point_list:
+            Mouse.position = x
+            Mouse.click(Button.left, 1)
+    else:
+        Mouse.click(Button.left, 1)
